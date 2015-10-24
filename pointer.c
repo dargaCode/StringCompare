e97509006a6d43fdc38175bdcf8f39c03f86b819
dargaCode <darga.code@gmail.com>
2015-10-24 15:53:45 -0400

    text prompts and skeleton for compare function

--- pointer.c
+++ pointer.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <cs50.h>
 
-int main(int argc, char * argv[])
+// function prototypes
+bool compare(char* a, char* b);
+
+int main(int argc, char* argv[])
 {
+    printf("Enter string a: ");
+    char* a = GetString();
+    printf("Enter string b: ");
+    char* b = GetString();
+    
+    bool match = compare(a, b);
+    printf("Do strings match? ");
+    printf(match ? "True\n" : "False\n"); 
+}
 
+bool compare(char* a, char* b)
+{
+    return false;
 }
